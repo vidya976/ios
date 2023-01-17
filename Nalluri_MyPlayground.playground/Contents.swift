fb60096d4e42fff576bb8bc7380aa2002a283fd0
Sri Vidya Nalluri <111886796+vidya976@users.noreply.github.com>
2023-01-17 10:47:42 -0600

    first class
    worksheet

---
+++ Nalluri_MyPlayground.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, playground"
+print(greeting)
+var x=10.0
+var y=20.0
+var z=x+y
+print(z)
+print(greeting,x,x+z,y)
+var w=z/y
+print(w)
+var str="race"
+print(greeting+str)
